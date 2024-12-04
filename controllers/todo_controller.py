c5476576c8d77e33538c736940879823ec0680b3
Randy Cullins <randycullins2018@gmail.com>
2024-12-03 19:47:14 -0700

    removed update order function

--- controllers/todo_controller.py
+++ controllers/todo_controller.py
@@ -61,36 +61,3 @@ def delete_todo(todo_id):
         return jsonify({"message" : "Unable to delete todo"}),400
 
     return jsonify({"message" : "Todo deleted"}),200
-
-def update_todo_order():
-    post_data = request.get_json()
-
-    new_order = post_data.get("newOrder", [])
-    if not new_order:
-        return jsonify({"message": "No new order provided"}), 400
-
-    try:
-        # Query existing todos from the database
-        todos = db.session.query(Todos).all()
-        
-        # Create a mapping of todo_id to todo object
-        todos_map = {str(todo.todo_id): todo for todo in todos}
-        
-        # Reorder the todos in the database based on the new order
-        reordered_todos = []
-        for item in new_order:
-            todo_id = str(item["todo_id"])  # Ensure todo_id is a string for comparison
-            if todo_id in todos_map:
-                reordered_todos.append(todos_map[todo_id])
-
-        if len(reordered_todos) != len(todos):
-            return jsonify({"message": "Order mismatch or invalid IDs"}), 400
-
-        # Save reordered todos
-        db.session.bulk_save_objects(reordered_todos)
-        db.session.commit()
-
-        return jsonify({"message": "Order updated successfully", "results": todos_schema.dump(reordered_todos)}), 201
-    except:
-        db.session.rollback()
-        return jsonify({"message": "Error updating order"}), 400
